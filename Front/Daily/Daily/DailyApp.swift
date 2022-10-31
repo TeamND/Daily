bb35ee9d22ea1c5ca80038e5f45900242fb4fd3a
seungyooooong <chadange@naver.com>
2022-10-31 10:22:00 +0900

    Create front project

---
+++ Front/Daily/Daily/DailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyApp.swift
+//  Daily
+//
+//  Created by 최승용 on 2022/10/31.
+//
+
+import SwiftUI
+
+@main
+struct DailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
