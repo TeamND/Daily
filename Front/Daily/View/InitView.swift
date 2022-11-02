34d8ccf745b5e195e839ef48edb23836558cc71b
seungyooooong <chadange@naver.com>
2022-11-03 00:34:34 +0900

    Add InitView

---
+++ Front/Daily/View/InitView.swift
@@ -0,0 +1,20 @@
+//
+//  InitView.swift
+//  Daily
+//
+//  Created by 최승용 on 2022/11/02.
+//
+
+import SwiftUI
+
+struct InitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InitView_Previews: PreviewProvider {
+    static var previews: some View {
+        InitView()
+    }
+}
