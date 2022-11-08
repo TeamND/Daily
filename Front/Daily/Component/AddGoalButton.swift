eeb286bcd0817e3aa4a955207d0a30883b29c975
seungyooooong <chadange@naver.com>
2022-11-08 13:22:44 +0900

    Add Component & Model
    Component: AddGoalButton
    Model: DateFormatter

---
+++ Front/Daily/Component/AddGoalButton.swift
@@ -0,0 +1,20 @@
+//
+//  AddGoalButton.swift
+//  Daily
+//
+//  Created by 최승용 on 2022/11/07.
+//
+
+import SwiftUI
+
+struct AddGoalButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddGoalButton_Previews: PreviewProvider {
+    static var previews: some View {
+        AddGoalButton()
+    }
+}
---
+++ Front/Daily/Model/DateFormatter.swift
@@ -0,0 +1,8 @@
+//
+//  DateFormatter.swift
+//  Daily
+//
+//  Created by 최승용 on 2022/11/07.
+//
+
+import Foundation
