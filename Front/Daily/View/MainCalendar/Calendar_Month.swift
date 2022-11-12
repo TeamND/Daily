b92e6b1e989bed78e47bd22af70dba5fc8fd7694
seungyooooong <chadange@naver.com>
2022-11-12 18:04:32 +0900

    Remove MonthCalendar

--- Front/Daily/View/MainCalendar/Calendar_Month.swift
+++ Front/Daily/View/MainCalendar/Calendar_Month.swift
@@ -14,8 +14,25 @@ struct Calendar_Month: View {
             WeeklyIndicator(weeks: $weeks)
                 .frame(maxWidth: .infinity, maxHeight: 30)
             CustomDivider(color: .black, height: 2, hPadding: 12)
-            MonthCalendar()
-                .frame(maxWidth: .infinity, maxHeight: .infinity)
+            VStack {
+                ForEach (0..<6) { rowIndex in
+                    HStack {
+                        ForEach (0..<7) { colIndex in
+                            let isToday = rowIndex == 1 && colIndex == 1
+                            DayOnMonth()
+                                .padding([.leading, .bottom, .trailing], 4)
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 5)
+                                        .stroke(.green, lineWidth: 2)
+                                        .opacity(isToday ? 1 : 0)
+                                }
+                        }
+                    }
+                    if rowIndex < 5 { CustomDivider(hPadding:8) }
+                }
+                Spacer()
+            }
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
         }
     }
 }
