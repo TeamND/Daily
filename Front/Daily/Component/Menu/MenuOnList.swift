4faf247d222a4d7c606ab4d8d671aacf46464265
seungyooooong <chadange@naver.com>
2022-11-27 08:42:26 +0900

    Add selected menu detail

--- Front/Daily/Component/Menu/MenuOnList.swift
+++ Front/Daily/Component/Menu/MenuOnList.swift
@@ -10,20 +10,36 @@ import SwiftUI
 struct MenuOnList: View {
     @StateObject var menu: Menu
     var body: some View {
-        HStack {
-            Text(menu.title)
-            Spacer()
-            Button {
-                menu.isSelected.toggle()
-            } label: {
-                HStack {
-                    Text(menu.selectedOption)
-                    Image(systemName: "chevron.right")
-                        .rotationEffect(menu.isSelected ? Angle(degrees: 90) : Angle(degrees: 0))
-                        .animation(.default, value: menu.isSelected)
+        ZStack(alignment: .top) {
+            VStack(spacing: 0) {
+                Rectangle()
+                    .frame(height: 30)
+                    .opacity(0)
+                VStack {
+                    WeekIndicator()
                 }
-                .foregroundColor(.mint)
+                .frame(height: menu.isSelected ? 70 : 0)
+                .opacity(menu.isSelected ? 1.0 : 0.0)
+                .scaleEffect(menu.isSelected ? 1 : 0.3, anchor: .top)
+                .animation(.default, value: menu.isSelected)
             }
+            .frame(height: menu.isSelected ? 100 : 30)
+            HStack {
+                Text(menu.title)
+                Spacer()
+                Button {
+                    menu.isSelected.toggle()
+                } label: {
+                    HStack {
+                        Text(menu.selectedOption)
+                        Image(systemName: "chevron.right")
+                            .rotationEffect(menu.isSelected ? Angle(degrees: 90) : Angle(degrees: 0))
+                            .animation(.default, value: menu.isSelected)
+                    }
+                    .foregroundColor(.mint)
+                }
+            }
+            .frame(height: 30)
         }
     }
 }
