41722d7b2b8c004dc33c31e39b82f9c9357d2c18
seungyooooong <chadange@naver.com>
2022-12-05 23:28:33 +0900

    Add isTapSelect

--- Front/Daily/Component/Calendar/MonthOnYear.swift
+++ Front/Daily/Component/Calendar/MonthOnYear.swift
@@ -8,8 +8,17 @@
 import SwiftUI
 
 struct MonthOnYear: View {
+    @State var archievements: [[Double]] = [
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0]
+    ]
     var monthIndex: Int
     var fontSize: CGFloat = 6
+    var isTapSelect: Bool = false
     var body: some View {
         VStack(alignment: .leading) {
                 Text(kMonths[monthIndex])
@@ -20,10 +29,24 @@ struct MonthOnYear: View {
                     ForEach (0..<7) { colIndex in
                         ZStack {
                             Image(systemName: "circle.fill")
-                                .font(.system(size: fontSize*2))
-                                .foregroundColor(.mint.opacity(0.4))
-                            Text("1")
-                                .font(.system(size: fontSize, weight: .bold))
+                                .font(.system(size: fontSize * 2))
+                                .foregroundColor(.mint.opacity(archievements[rowIndex][colIndex]))
+                            if isTapSelect {
+                                Button {
+                                    if archievements[rowIndex][colIndex] == 0 {
+                                        archievements[rowIndex][colIndex] = 0.4
+                                    } else {
+                                        archievements[rowIndex][colIndex] = 0
+                                    }
+                                } label: {
+                                    Text("1")
+                                        .foregroundColor(.black)
+                                        .font(.system(size: fontSize, weight: .bold))
+                                }
+                            } else {
+                                Text("1")
+                                    .font(.system(size: fontSize, weight: .bold))
+                            }
                         }
                     }
                 }
