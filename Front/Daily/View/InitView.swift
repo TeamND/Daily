ed3178c462eaea14f7e4d91464b98bebead016fa
seungyooooong <chadange@naver.com>
2022-12-15 15:26:16 +0900

    Complete initView

--- Front/Daily/View/InitView.swift
+++ Front/Daily/View/InitView.swift
@@ -11,28 +11,36 @@ import Combine
 struct InitView: View {
     @Binding var isLoading: Bool
     var body: some View {
-        Image(systemName: "d.circle.fill")
-            .resizable()
-            .renderingMode(.template)
-            .aspectRatio(contentMode: .fit)
-            .frame(width: 280, height: 280)
-            .foregroundColor(.mint)
-            .padding([.bottom], 40)
-            .task {
-                do {
-                    getUserInfo(userID: "test123") { (success, data) in
-                        guard let uid = data["uid"]! as? Int else { return }
-                        guard let set_startday = data["set_startday"]! as? Int else { return }
-                        guard let set_language = data["set_language"]! as? String else { return }
-                        guard let set_dateorrepeat = data["set_dateorrepeat"]! as? String else { return }
-                        userInfo = UserInfo(uid: uid, set_startday: set_startday, set_language: set_language, set_dateorrepeat: set_dateorrepeat)
-                    }
-                    // 임시 타이머
-                    Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { timer in
-                        if userInfo.uid > 0 { isLoading = false }
-                        else { print("An error has occured while getUserInfo") }
+        VStack(spacing: 40) {
+            Image(systemName: "d.circle.fill")
+                .resizable()
+                .frame(width: 280, height: 280)
+                .foregroundColor(.mint)
+                .task {
+                    do {
+                        getUserInfo(userID: "test123") { (success, data) in
+                            guard let uid = data["uid"]! as? Int else { return }
+                            guard let set_startday = data["set_startday"]! as? Int else { return }
+                            guard let set_language = data["set_language"]! as? String else { return }
+                            guard let set_dateorrepeat = data["set_dateorrepeat"]! as? String else { return }
+                            userInfo = UserInfo(uid: uid, set_startday: set_startday, set_language: set_language, set_dateorrepeat: set_dateorrepeat)
+                        }
+                        // 임시 타이머
+                        Timer.scheduledTimer(withTimeInterval: 1, repeats: false) { timer in
+                            if userInfo.uid > 0 { isLoading = false }
+                            else { print("An error has occured while getUserInfo") }
+                        }
                     }
                 }
-            }
+            Text("Design 🎨, Record 📝\n\n\t\t, and Check 👏 'Daily'!!")
+                .font(.headline)
+        }
+    }
+}
+
+
+struct InitView_Previews: PreviewProvider {
+    static var previews: some View {
+        InitView(isLoading: .constant(true))
     }
 }
