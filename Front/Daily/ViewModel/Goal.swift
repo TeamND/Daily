4981bc26d808648a8b3a1e12088f0fb192252e24
seungyooooong <chadange@naver.com>
2023-01-14 13:39:21 +0900

    Remove GoalList

--- Front/Daily/ViewModel/Goal.swift
+++ Front/Daily/ViewModel/Goal.swift
@@ -7,46 +7,31 @@
 
 import Foundation
 
-var goalList: [Goal] = [Goal(), Goal()]
+class Goal: ObservableObject {
+    @Published var uid: Int
+    @Published var user_uid: Int
+    @Published var content: String
+    @Published var type: String
+    @Published var symbol: String
+    @Published var start_date: String
+    @Published var end_date: String
+    @Published var cycle_type: String
+    @Published var cycle_date: String
+    @Published var goal_time: String
+    @Published var goal_count: Int
 
-class Goal: ObservableObject, Identifiable {
-    @Published var id: UUID = UUID()
-    @Published var type: String = "timer"   // check, count, timer
-    @Published var beforeSymbol: String = "dumbbell"
-    @Published var afterSymbol: String = "dumbbell.fill"
-    @Published var content: String = "6시 기상"
-    @Published var isSuccess: Bool = false
-    @Published var countOrTime: String = "횟수"   // 횟수, 시간
-    @Published var recordCount: Int = 2
-    @Published var goalCount: Int = 5
-    @Published var isStart: Bool = false
-    @Published var recordTime: String = "1:00:05"
-    @Published var goalTimeIndex: Int = 3
-
-
-//    @Published var uid: Int
-//    @Published var user_uid: Int
-//    @Published var content: String
-//    @Published var symbol: String
-//    @Published var start_date: String
-//    @Published var end_date: String
-//    @Published var cycle_type: String
-//    @Published var cycle_date: String
-//    @Published var type: String
-//    @Published var goal_time: String
-//    @Published var goal_count: Int
-//
-//    init(uid: Int, user_uid: Int, content: String, symbol: String, start_date: String, end_date: String, cycle_type: String, cycle_date: String, type: String, goal_time: String, goal_count: Int) {
-//        self.uid = uid
-//        self.user_uid = user_uid
-//        self.content = content
-//        self.symbol = symbol
-//        self.start_date = start_date
-//        self.end_date = end_date
-//        self.cycle_type = cycle_type
-//        self.cycle_date = cycle_date
-//        self.type = type
-//        self.goal_time = goal_time
-//        self.goal_count = goal_count
-//    }
+    init(uid: Int, user_uid: Int, content: String, type: String, symbol: String,
+         start_date: String, end_date: String, cycle_type: String, cycle_date: String, goal_time: String, goal_count: Int) {
+        self.uid = uid
+        self.user_uid = user_uid
+        self.content = content
+        self.type = type
+        self.symbol = symbol
+        self.start_date = start_date
+        self.end_date = end_date
+        self.cycle_type = cycle_type
+        self.cycle_date = cycle_date
+        self.goal_time = goal_time
+        self.goal_count = goal_count
+    }
 }
