b3dd9794efbd5a8d4ae1acd6be850dc2cb8938df
e3hope <e3hope@naver.com>
2023-05-12 09:33:26 +0900

    update phone_uid type

--- Back/model.py
+++ Back/model.py
@@ -7,7 +7,7 @@ db = flask_sqlalchemy.SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'user'
     uid = db.Column(db.Integer, primary_key=True)
-    phone_uid = db.Column(db.Integer)
+    phone_uid = db.Column(db.String(100))
     set_startday = db.Column(db.Integer, default=0)
     set_language = db.Column(db.String(100), default='korea')
     set_dateorrepeat = db.Column(db.String(100), default='date')
