a619a450fc7753121cf01b20302afc074f4d958e
chadange <chadange@naver.com>
2024-05-29 17:15:53 +0900

    Find error

---
+++ Front/Daily/Controller/PushNoticeManager.swift
@@ -0,0 +1,8 @@
+//
+//  PushNoticeManager.swift
+//  Daily
+//
+//  Created by 최승용 on 5/29/24.
+//
+
+import Foundation
---
+++ Front/Daily/Extensions/AppDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  AppDelegate.swift
+//  Daily
+//
+//  Created by 최승용 on 5/29/24.
+//
+
+import Foundation
