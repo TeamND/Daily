57154eb6c2bde37520c851b1308474164e545c8e
chadange <chadange@naver.com>
2024-07-01 16:20:38 +0900

    Apply temporary ViewThatFits in CalendarSheet

--- Front/Daily/Sheet/CalendarSheet.swift
+++ Front/Daily/Sheet/CalendarSheet.swift
@@ -14,24 +14,47 @@ struct CalendarSheet: View {
     @State var selectedDate: Date = Date()
     
     var body: some View {
-        VStack {
-            DatePicker("", selection: $selectedDate, displayedComponents: [.date])
-                .datePickerStyle(.graphical)
-            HStack {
-                Text("\(String(selectedDate.year))년 \(String(format: "%02d", selectedDate.month))월 \(String(format: "%02d", selectedDate.day))일")
-                Spacer()
-                Button {
-                    currentDate = selectedDate
-                    calendarViewModel.setCurrentYear(year: selectedDate.year)
-                    calendarViewModel.setCurrentMonth(month: selectedDate.month)
-                    calendarViewModel.setCurrentDay(day: selectedDate.day)
-                    self.presentationMode.wrappedValue.dismiss()
-                } label: {
-                    Text("변경")
+        ViewThatFits(in: .vertical) {
+            VStack {
+                DatePicker("", selection: $selectedDate, displayedComponents: [.date])
+                    .datePickerStyle(.graphical)
+                HStack {
+                    Text("\(String(selectedDate.year))년 \(String(format: "%02d", selectedDate.month))월 \(String(format: "%02d", selectedDate.day))일")
+                    Spacer()
+                    Button {
+                        currentDate = selectedDate
+                        calendarViewModel.setCurrentYear(year: selectedDate.year)
+                        calendarViewModel.setCurrentMonth(month: selectedDate.month)
+                        calendarViewModel.setCurrentDay(day: selectedDate.day)
+                        self.presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Text("변경")
+                    }
+                }
+                .padding(.horizontal)
+                .buttonStyle(.borderedProminent)
+            }
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack {
+                    DatePicker("", selection: $selectedDate, displayedComponents: [.date])
+                        .datePickerStyle(.graphical)
+                    HStack {
+                        Text("\(String(selectedDate.year))년 \(String(format: "%02d", selectedDate.month))월 \(String(format: "%02d", selectedDate.day))일")
+                        Spacer()
+                        Button {
+                            currentDate = selectedDate
+                            calendarViewModel.setCurrentYear(year: selectedDate.year)
+                            calendarViewModel.setCurrentMonth(month: selectedDate.month)
+                            calendarViewModel.setCurrentDay(day: selectedDate.day)
+                            self.presentationMode.wrappedValue.dismiss()
+                        } label: {
+                            Text("변경")
+                        }
+                    }
+                    .padding(.horizontal)
+                    .buttonStyle(.borderedProminent)
                 }
             }
-            .padding(.horizontal)
-            .buttonStyle(.borderedProminent)
         }
         .accentColor(Color("CustomColor"))
         .onAppear {
