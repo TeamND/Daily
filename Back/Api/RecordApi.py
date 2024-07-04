f3544abb705f3897ba11b51f1b737529ce88005b
e3hope <e3hope@naver.com>
2024-07-04 13:27:53 +0900

    Separate record related API and Add record table of contents API and Override goal, record deletion

---
+++ Back/Api/RecordApi.py
@@ -0,0 +1,80 @@
+from flask import request
+from flask_restx import Resource, Api, Namespace
+from sqlalchemy import extract,func
+from model import db,Goal,Record
+from Api.UserApi import UserApi
+import datetime
+
+class recordApi(Resource):
+    def Update(uid,data):
+        result = db.session.get(Record,uid)
+        
+        if result:
+            UserApi.LastTime('record',uid)
+            try:
+                for k,v in data.items():
+                    setattr(result, k, v)
+                db.session.commit()
+                return {
+                    'code': '00',
+                    'message': '수정에 성공했습니다.'
+                }, 00
+            except Exception as e:
+                db.session.rollback()
+                return {
+                    'code': '99',
+                    'message': e
+                }, 99
+        else: 
+           return {
+                'code': '99',
+                'message': '조회된 데이터가 없습니다.'
+            }, 99
+        
+
+    def Delete(uid):
+        try:
+            result = db.session.get(Record,uid)
+            if result:
+                UserApi.LastTime('record',uid)
+                db.session.delete(result)
+                db.session.commit()
+                return {
+                    'code': '00',
+                    'message': '삭제에 성공했습니다.'
+                }, 00
+            else:
+                return {
+                    'code': '99',
+                    'message': '조회된 데이터가 없습니다.'
+                }, 99
+        except Exception as e:
+            db.session.rollback()
+            return {
+                'code': '99',
+                'message': e
+            }, 99
+        
+
+    def DeleteAll(uid):
+        try:
+            result = db.session.get(Goal,uid)
+            if result:
+                UserApi.LastTime('goal',uid)
+                Record.query.filter(Record.goal_uid == uid,Record.date > datetime.datetime.today()).delete()
+                db.session.commit()
+                return {
+                    'code': '00',
+                    'message': '삭제에 성공했습니다.'
+                }, 00
+            else:
+                return {
+                    'code': '99',
+                    'message': '조회된 데이터가 없습니다.'
+                }, 99
+        except Exception as e:
+            db.session.rollback()
+            return {
+                'code': '99',
+                'message': e
+            }, 99
---
+++ Back/Controller/RecordController.py
@@ -0,0 +1,48 @@
+from flask import request
+from flask_restx import Resource, Api, Namespace, fields, reqparse
+from Api.RecordApi import recordApi
+import json
+
+record = Namespace(
+    name="record",
+    description="기록를 만들기 위해 사용하는 API",
+)
+
+model = record.model('기록', strict=True, model={
+    'goal_uid': fields.Integer(title='목표 고유번호', required=True),
+    'date': fields.String(title='날짜', required=True),
+    'order': fields.Integer(title='순서', default=0, required=True),
+    'issuccess': fields.String(title='성공여부', default='잡담', required=True),
+    'record_count': fields.String(title='현재 횟수', default='title', required=True),
+    'record_time': fields.String(title='성공 시간', default='date', required=True),
+    'start_time': fields.String(title='시작 시간'),
+})
+
+day_column = reqparse.RequestParser()
+day_column.add_argument('date', type=str, default='2024-07-24', help='날짜')
+
+@record.route('/<int:uid>')
+class RecordUD(Resource):
+    @record.doc(responses={00: 'Success'})
+    @record.doc(responses={99: 'Failed'})
+    def put(self,uid):
+        '''기록을 수정한다.'''
+        data = request.get_data()
+        data = data.decode('UTF-8')
+        data = json.loads(data)
+        return recordApi.Update(uid,data)
+    
+    @record.doc(responses={00: 'Success'})
+    @record.doc(responses={99: 'Failed'})    
+    def delete(self,uid):
+        '''기록을 삭제한다.'''
+        return recordApi.Delete(uid)
+    
+
+@record.route('/deleteAll/<int:goal_uid>')
+class DeleteAll(Resource):
+    @record.doc(responses={00: 'Success'})
+    @record.doc(responses={99: 'Failed'})       
+    def delete(self,goal_uid):
+        '''기록의 일괄 삭제 한다.'''
+        return recordApi.DeleteAll(goal_uid)    
