21f9e85fc67fe12a9042af701926aeee5087ccc9
e3hope <e3hope@naver.com>
2024-07-09 15:19:49 +0900

    Includes zero value correction to modify record

--- Back/Api/RecordApi.py
+++ Back/Api/RecordApi.py
@@ -11,11 +11,11 @@ class recordApi(Resource):
         
         if result:
             UserApi.LastTime('record',uid)
-            if 'record_count' in data and data['record_count']:
+            if 'record_count' in data:
                 goal = Goal.query.filter(Goal.uid == result.goal_uid).first()
                 data['issuccess'] = True if goal.goal_count == data['record_count'] else False
             
-            if 'record_time' in data and data['record_time']:
+            if 'record_time' in data:
                 goal = Goal.query.filter(Goal.uid == result.goal_uid).first()
                 data['issuccess'] = True if goal.goal_time <= data['record_time'] else False
 
