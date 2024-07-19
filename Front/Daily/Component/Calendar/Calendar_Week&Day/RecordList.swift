7cb201fea578a4a9e955792422d370912c726032
seungyooooong <chadange@naver.com>
2024-07-20 02:13:54 +0900

    version up

--- Front/Daily/Component/Calendar/Calendar_Week&Day/RecordList.swift
+++ Front/Daily/Component/Calendar/Calendar_Week&Day/RecordList.swift
@@ -41,8 +41,12 @@ struct RecordList: View {
                         if record.cycle_type.wrappedValue == "repeat" {
                             if record.parent_uid.wrappedValue == nil {
                                 Menu {
-                                    NavigationLink {
-                                        ModifyGoalView(userInfoViewModel: userInfoViewModel, calendarViewModel: calendarViewModel, record: record, modifyGoalModel: modifyGoalModel(record: record.wrappedValue), isAll: false)
+                                    Button {
+                                        withAnimation {
+                                            alertViewModel.showToast(message: commingSoonToastMessage)
+                                        }
+//                                    NavigationLink {
+//                                        ModifyGoalView(userInfoViewModel: userInfoViewModel, calendarViewModel: calendarViewModel, record: record, modifyGoalModel: modifyGoalModel(record: record.wrappedValue), isAll: false)
                                     } label: {
                                         Text("단일 수정")
                                     }
