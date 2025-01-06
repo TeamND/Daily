c0e0633f89c66f0e61e05fdfeb886f4ce6af170c
seungyooooong <chadange@naver.com>
2025-01-06 16:15:41 +0900

    feat #112: WeeklySummary 나머지 구현

--- Front/Daily/Presentation/Calendar/CalendarDayView.swift
+++ Front/Daily/Presentation/Calendar/CalendarDayView.swift
@@ -86,105 +86,43 @@ struct DailyWeeklySummary: View {
     
     private var ratingsForChart: [RatingOnWeekModel] {
         let calendar = Calendar.current
-        var ratingsForChart: [RatingOnWeekModel] = Array(repeating: RatingOnWeekModel(day: "", rating: 0.0), count: 7)
-        
-        for index in 0 ..< ratingsForChart.count {
-            ratingsForChart[index].day = DayOfWeek.text(for: index) ?? ""
-        }
-        
-        for record in records {
-            if let dayIndex = calendar.dateComponents([.weekday], from: record.date).weekday {
-                let index = dayIndex - 1
-                if record.isSuccess { ratingsForChart[index].rating += 1 }
-            }
+        var ratingsForChart = (0 ..< 7).map { index in
+            RatingOnWeekModel(day: DayOfWeek.text(for: index) ?? "", rating: 0.0)
         }
         
         let recordsByDay = Dictionary(grouping: records) { record -> Int in
             calendar.dateComponents([.weekday], from: record.date).weekday! - 1
         }
         
-        for (index, totalRecords) in recordsByDay {
-            ratingsForChart[index].rating = ratingsForChart[index].rating / Double(totalRecords.count) * 100
+        for (index, record) in recordsByDay {
+            let successCount = record.filter { $0.isSuccess }.count
+            ratingsForChart[index].rating = Double(successCount) / Double(record.count) * 100
         }
         
         return ratingsForChart
     }
     
     private var ratingOfWeek: Int {
-        return 90
+        let calendar = Calendar.current
+        let today = calendar.startOfDay(for: Date())
+        let validRecords = records.filter { record in
+            calendar.startOfDay(for: record.date) <= today
+        }
+        
+        let totalRecords = validRecords.count
+        guard totalRecords > 0 else { return 0 }
+        
+        let successCount = validRecords.filter { $0.isSuccess }.count
+        let successRate = Double(successCount) / Double(totalRecords) * 100
+        
+        return Int(round(successRate))
     }
     
     var body: some View {
         VStack(spacing: .zero) {
             Spacer()
-            Group {
-                VStack(spacing: .zero) {
-                    RoundedRectangle(cornerRadius: 10)
-                        .fill(Colors.reverse.opacity(0.3))
-                        .frame(width: CGFloat.fontSize * 5, height: CGFloat.fontSize * 0.8)
-                        .padding(CGFloat.fontSize)
-                    Text("주간 요약")
-                        .font(.system(size: CGFloat.fontSize * 2.5, weight: .bold))
-                }
-                .padding(.horizontal, CGFloat.fontSize * 3)
-                .padding(.bottom, CGFloat.fontSize * 3)
-                .background(Colors.background)
-                .cornerRadius(20)
-                .zIndex(1)
-            }
-            .padding(.horizontal, 20)
-            .padding(.bottom, -20)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            Group {
-                VStack(alignment: .center, spacing: CGFloat.fontSize * 3) {
-                    Text("\(dailyCalendarViewModel.currentDate.month)월 \(dailyCalendarViewModel.currentDate.weekOfMonth)주차 목표 달성률")
-                        .font(.system(size: CGFloat.fontSize * 2.5, weight: .bold))
-                    Chart {
-                        ForEach(ratingsForChart) { data in
-                            BarMark(
-                                x: .value("Day", data.day),
-                                y: .value("Rating", data.rating)
-                            )
-                            .opacity(0.3)
-                            .annotation(position: .top, alignment: .center) {
-                                Text(data.rating.percentFormat())
-                                    .font(.system(size: CGFloat.fontSize * 1.5))
-                            }
-                            if ratingOfWeek > 0 {
-                                RuleMark(
-                                    y: .value("RatingOfWeek", ratingOfWeek)
-                                )
-                                .lineStyle(StrokeStyle(lineWidth: 2))
-                                .annotation(
-                                    position: .top, alignment: .leading
-//                                    position:
-//                                        calendarViewModel.ratingOnWeekForCharts[0].rating == calendarViewModel.ratingOfWeek ||
-//                                    calendarViewModel.ratingOnWeekForCharts[1].rating == calendarViewModel.ratingOfWeek ||
-//                                    calendarViewModel.ratingOnWeekForCharts[2].rating == calendarViewModel.ratingOfWeek ? .bottom : .top
-//                                    , alignment: .leading
-                                ) {
-                                    Text(" 주간 달성률 : \(ratingOfWeek)%")
-                                        .font(.system(size: CGFloat.fontSize * 2, weight: .bold))
-                                }
-                            }
-                        }
-                    }
-                    .chartYScale(domain: 0 ... 100)
-                    .animation(.none, value: dailyCalendarViewModel.isShowWeeklySummary)
-                    .foregroundStyle(Colors.reverse)
-                    .frame(height: 200)
-                    if true {
-                        Text("* 오늘 이후의 목표는 주간 달성률 계산에 포함되지 않습니다.")
-                            .font(.system(size: CGFloat.fontSize * 1.5))
-                            .frame(maxWidth: .infinity, alignment: .trailing)
-                    }
-                }
-                .padding(CGFloat.fontSize * 5)
-                .frame(height: 350)
-            }
-            .font(.system(size: CGFloat.fontSize * 3))
-            .background(Colors.background)
-            .cornerRadius(20)
+            weeklySummaryHeader
+            weeklySummaryBody
         }
         .ignoresSafeArea()
         .onTapGesture {
@@ -211,5 +149,70 @@ struct DailyWeeklySummary: View {
                 Color.black.opacity(0.5).onTapGesture { withAnimation { dailyCalendarViewModel.isShowWeeklySummary = false } }
             }
         }
+    }
+    
+    private var weeklySummaryHeader: some View {
+        Group {
+            VStack(spacing: .zero) {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(Colors.reverse.opacity(0.3))
+                    .frame(width: CGFloat.fontSize * 5, height: CGFloat.fontSize * 0.8)
+                    .padding(CGFloat.fontSize)
+                Text("주간 요약")
+                    .font(.system(size: CGFloat.fontSize * 2.5, weight: .bold))
+            }
+            .padding(.horizontal, CGFloat.fontSize * 3)
+            .padding(.bottom, CGFloat.fontSize * 3)
+            .background(Colors.background)
+            .cornerRadius(20)
+            .zIndex(1)
+        }
+        .padding(.horizontal, 20)
+        .padding(.bottom, -20)
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
+    private var weeklySummaryBody: some View {
+        Group {
+            VStack(alignment: .center, spacing: CGFloat.fontSize * 3) {
+                Text("\(dailyCalendarViewModel.currentDate.month)월 \(dailyCalendarViewModel.currentDate.weekOfMonth)주차 목표 달성률")
+                    .font(.system(size: CGFloat.fontSize * 2.5, weight: .bold))
+                Chart {
+                    ForEach(ratingsForChart) { data in
+                        BarMark(
+                            x: .value("Day", data.day),
+                            y: .value("Rating", data.rating)
+                        )
+                        .opacity(0.3)
+                        .annotation(position: .top, alignment: .center) {
+                            Text(data.rating.percentFormat())
+                                .font(.system(size: CGFloat.fontSize * 1.5))
+                        }
+                        if ratingOfWeek > 0 {
+                            RuleMark(y: .value("RatingOfWeek", ratingOfWeek))
+                                .lineStyle(StrokeStyle(lineWidth: 2))
+                                .annotation(
+                                    position: ratingsForChart[0].rating == Double(ratingOfWeek) || ratingsForChart[1].rating == Double(ratingOfWeek) || ratingsForChart[2].rating == Double(ratingOfWeek) ? .bottom : .top, alignment: .leading
+                                ) {
+                                    Text(" 주간 달성률 : \(ratingOfWeek)%")
+                                        .font(.system(size: CGFloat.fontSize * 2, weight: .bold))
+                                }
+                        }
+                    }
+                }
+                .chartYScale(domain: 0 ... 100)
+                .animation(.none, value: dailyCalendarViewModel.isShowWeeklySummary)
+                .foregroundStyle(Colors.reverse)
+                .frame(height: 200)
+                Text("* 오늘 이후의 목표는 주간 달성률 계산에 포함되지 않습니다.")
+                    .font(.system(size: CGFloat.fontSize * 1.5))
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+            }
+            .padding(CGFloat.fontSize * 5)
+            .frame(height: 350)
+        }
+        .font(.system(size: CGFloat.fontSize * 3))
+        .background(Colors.background)
+        .cornerRadius(20)
     }
 }
