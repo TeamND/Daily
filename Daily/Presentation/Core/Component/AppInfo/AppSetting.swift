913154bfcd497791e539d390f60a8752d25d2012
seungyooooong <chadange@naver.com>
2025-02-23 19:03:08 +0900

    chore #116: startDay 작업 일시 중지

--- Daily/Presentation/Core/Component/AppInfo/AppSetting.swift
+++ Daily/Presentation/Core/Component/AppInfo/AppSetting.swift
@@ -11,8 +11,8 @@ struct AppSetting: View {
     var body: some View {
         VStack {
             GroupBox {
-                AppInfoContent(name: "StartDay", settingType: .startDay)
-                AppInfoContent(name: "Language", settingType: .language)
+//                AppInfoContent(name: "StartDay", settingType: .startDay)
+//                AppInfoContent(name: "Language", settingType: .language)
                 AppInfoContent(name: "Initial CalendarType", settingType: .calendarType)
             } label: {
                 AppInfoLabel(labelText: "Setting", labelImage: "gear")
