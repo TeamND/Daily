5b5a1d0b9d956b2baa46a1d200d1fe9c7951afb7
seungyooooong <chadange@naver.com>
2025-07-23 15:24:27 +0900

    refactor #168: extension 정리

--- Daily/Extension/UINavigationController+extension.swift
+++ Daily/Extension/UINavigationController+extension.swift
@@ -5,7 +5,6 @@
 //  Created by seungyooooong on 10/28/24.
 //
 
-import Foundation
 import UIKit
 
 extension UINavigationController: @retroactive ObservableObject, @retroactive UIGestureRecognizerDelegate {
