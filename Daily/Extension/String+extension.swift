8106de8486e727b3b67ea936e6992bc90ee0ca80
seungyooooong <chadange@naver.com>
2025-07-23 15:24:27 +0900

    refactor #168: extension 정리

--- Daily/Extension/String+extension.swift
+++ Daily/Extension/String+extension.swift
@@ -16,23 +16,3 @@ extension String {
         return dateFormatter.date(from: self)
     }
 }
-
-// MARK: - AttributedString
-extension String {
-    func accent(keywords: [String], style: (inout AttributeContainer) -> Void) -> AttributedString {
-        var attributedString = AttributedString(self)
-        
-        for keyword in keywords {
-            if let range = self.range(of: keyword) {
-                let nsRange = NSRange(range, in: self)
-                if let attributedRange = Range(nsRange, in: attributedString) {
-                    var container = AttributeContainer()
-                    style(&container)
-                    attributedString[attributedRange].setAttributes(container)
-                }
-            }
-        }
-        
-        return attributedString
-    }
-}
